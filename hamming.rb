06cf07a96039466b82ca6d0008aea6da27ea4684
Valinda Chan <valinda.chan@gmail.com>
2017-08-15 10:43:05 -0400

    Clean up code

--- hamming.rb
+++ hamming.rb
@@ -5,7 +5,7 @@ class Hamming
     else
       hammingDistance = 0
       strand_two = strand_two.split("")
-      strand_one.each_char.with_index(0) do |char, index|
+      strand_one.each_char.with_index do |char, index|
         if strand_two[index] != char.to_s
           hammingDistance += 1
         end
